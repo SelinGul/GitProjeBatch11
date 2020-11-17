88334420ec3b736cf2d7f725a3682a88d67a2014
Selin <selinrose79@gmail.com>
2020-11-17 21:34:03 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -10,7 +10,7 @@ public class BirinciGun {
 
 		System.out.println("11.05-local ");
                 System.out.println("11.23-github.com ");
-		
+		System.out.println("Ben buradan conflict beklemiyorum");
 		
 		
 	}
