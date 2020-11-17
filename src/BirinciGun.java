d9cae9a1a6b911700baec872bd6ffec49c8a340a
SelinGul <74491729+SelinGul@users.noreply.github.com>
2020-11-17 21:50:37 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -8,8 +8,9 @@ public class BirinciGun {
 		System.out.println("10:26 -Ikinci gun");
 		System.out.println("10:35 -Ikinci gun");
 		System.out.println("11.05-local ");
-        System.out.println("11.23-github.com ");
+                System.out.println("11.23-github.com ");
 		System.out.println("Ben buradan conflict beklemiyorum");
+                System.out.println("Yepyeni bir satir");
 
               
                
