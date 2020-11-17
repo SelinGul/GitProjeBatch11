b91bfe0982dee96eb26f7a3c7ca1d7354ac2254b
Selin <selinrose79@gmail.com>
2020-11-17 20:28:23 +0100

    ikinci gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,8 +4,8 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11:57 - Birinci Gün");
 		System.out.println("Ikinci commit");
-		System.out.println("10:11-Ikinci gun);
-		
+		System.out.println("10:11-Ikinci gun");
+		System.out.println("10:26 -Ikinci gun");
 		
 		
 		
