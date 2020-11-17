e6f214559f6d5b5f982942878e3827798b793abc
Selin <selinrose79@gmail.com>
2020-11-17 21:16:08 +0100

    conflict cozuldu

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,7 +7,7 @@ public class BirinciGun {
 		System.out.println("10:11-Ikinci gun");
 		System.out.println("10:26 -Ikinci gun");
 		System.out.println("10:35 -Ikinci gun");
-		
+		System.out.println("11.05-local ");
 		
 		
 		
